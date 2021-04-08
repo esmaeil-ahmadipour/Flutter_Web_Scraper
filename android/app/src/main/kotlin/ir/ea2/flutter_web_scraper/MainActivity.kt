c89605b6db91a086f590bfb26c0db705c6aa3aeb
Esmaeil Ahmadipour <software8686@gmail.com>
2021-04-08 12:22:28 +0430

    Initial commit

---
+++ android/app/src/main/kotlin/ir/ea2/flutter_web_scraper/MainActivity.kt
@@ -0,0 +1,6 @@
+package ir.ea2.flutter_web_scraper
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_scraper/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
